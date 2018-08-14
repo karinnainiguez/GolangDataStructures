c139f6a2ce6b199d6db28421056ffd251b219c65
Karinna Iniguez <kngz@amazon.com>
2018-08-14 16:57:12 -0700

    implement max method for linked list that returns max value of linked list (not node)

--- linkedlist.go
+++ linkedlist.go
@@ -30,6 +30,18 @@ func (l *LinkedList) isFound(value int) bool {
 	return false
 }
 
+// 3. return max value in list (return data, not node)
+func (l *LinkedList) max() int {
+	max := l.head.data
+	current := l.head
+	for current != nil {
+		if current.data > max {
+			max = current.data
+		}
+	}
+	return max
+}
+
 func main() {
 	fmt.Println("Working")
 }
