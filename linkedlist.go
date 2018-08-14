599fea5778c3f3447db9d0074b66643899665e67
Karinna Iniguez <kngz@amazon.com>
2018-08-14 16:55:47 -0700

    implement add method to add node to linked list with given input data

--- linkedlist.go
+++ linkedlist.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+type Node struct {
+	data     int
+	nextNode *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+// 1. Add new node to list based on input data
+func (l *LinkedList) add(inputData int) {
+	newNode := Node{data: inputData, nextNode: l.head}
+	l.head = &newNode
+}
+
 func main() {
 	fmt.Println("Working")
 }
