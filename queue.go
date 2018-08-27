ded3a31c01cd4b34a31bb75e2ef186215deb5225
Karinna Iniguez <kngz@amazon.com>
2018-08-27 16:06:26 -0700

    Stub out necessary functions for queue

--- queue.go
+++ queue.go
@@ -9,3 +9,33 @@ type QueueNode struct {
 	data     int
 	nextNode *QueueNode
 }
+
+func (q *Queue) enqueue() {
+
+}
+
+func (q *Queue) dequeue() {
+
+}
+
+func (q *Queue) Empty() bool {
+	// returns true is the data structure is empty,
+	// false otherwise
+	return false
+}
+
+func (q *Queue) Size() {
+
+}
+
+func (q *Queue) Front() {
+
+}
+
+func (q *Queue) Min() {
+
+}
+
+func (q *Queue) Max() {
+
+}
