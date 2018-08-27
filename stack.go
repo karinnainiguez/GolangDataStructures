0e8908b667a405b62eaf80719c9519aa9984b762
Karinna Iniguez <kngz@amazon.com>
2018-08-27 16:55:44 -0700

    Implement pop function

--- stack.go
+++ stack.go
@@ -1,1 +1,20 @@
 package main
+
+type Stack struct {
+	top *StackNode
+}
+
+type StackNode struct {
+	data     int
+	nextNode *StackNode
+}
+
+func (s *Stack) push(data int) {
+
+}
+
+func (s *Stack) pop() StackNode {
+	nodeToRemove := s.top
+	s.top = s.top.nextNode
+	return *nodeToRemove
+}
